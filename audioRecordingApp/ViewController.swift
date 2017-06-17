914b57cb6884d3af1818014a2548937a3428d081
Claire Davis <claire.a.davis@vanderbilt.edu>
2017-06-17 18:58:55 +0200

    Initial Commit

---
+++ audioRecordingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  audioRecordingApp
+//
+//  Created by Claire Davis on 6/17/17.
+//  Copyright © 2017 iXperience. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
